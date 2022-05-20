7946108731d8a3145e994224ae734c7888891bce
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-05-19 19:39:39 -0500

    Reorganization

--- Scripts/Modules/params.py
+++ Scripts/Modules/params.py
@@ -3,16 +3,22 @@ from os import makedirs
 
 def get_params() -> dict:
     params = {
-        "path data": "Data",
-        "path results": "Results",
-        "path graphics": "Graphics",
+        "path data": "../Data",
+        "path results": "../Results",
+        "path models": "../Models",
+        "path graphics": "../Graphics",
         "news model": "news_model.h5",
         "yahoo model": "yahoo_model.h5",
+        "path yahoo": "Yahoo",
         "yahoo data": "train.csv",
         "yahoo topics": "classes.txt",
+        "path tripadvisor": "Tripadvisor",
+        "tripadvisor model": "tripadvisor_model.h5",
+        "tripadvisor data": "opinions.csv",
     }
     mkdir(params["path results"])
     mkdir(params["path graphics"])
+    mkdir(params["path models"])
     return params
 
 
